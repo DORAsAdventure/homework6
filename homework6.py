6cc3d80c26bcb7f1f5708cf08803f98b86a47a51
DORAsAdventure <168351749+DORAsAdventure@users.noreply.github.com>
2024-05-04 06:31:57 +0800

    Add files via upload

---
+++ homework6.py
@@ -0,0 +1,12 @@
+my_list=['mango','apple','banana','kiwi','orange']
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2]='grape'
+print(my_list)
+my_dict={'mango':'манго','apple':'яблоко','banana':'банан','kiwi':'киви','orange':'апельсин'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict['mango']='национальный фрукт Индии'
+my_dict['coconut']='кокос'
+print(my_dict)
